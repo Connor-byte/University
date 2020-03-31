8f6f57357f72b7c3cd4ca091ab159ab29944d91a
Connor-byte <62945108+Connor-byte@users.noreply.github.com>
2020-03-31 18:35:18 +0100

    Task 4 Code
    Code for Task 4
    Part A, B and D

---
+++ Dog.java
@@ -0,0 +1,28 @@
+//Start of project//
+//**********************************************//
+//* Author: 1807192                            *//
+//* Week: 6                                    *//
+//* Tutorial Task: Animal Action               *//          
+//**********************************************//
+
+//Loads package//
+package nor.animal;
+
+//Declares class and extends to Animal class//
+public class Dog extends Animal {
+	
+	public void bark() {
+		
+		//Outputs required string//
+		System.out.println("Woof!");
+		
+	}//End of public void bark//
+	
+	public void move() {
+		
+		//Outputs required string//
+		System.out.println("Running");
+		
+	}//End of public void move//
+}//End of class Dog//
+//End of project//
